0acaf8c81e98792fc4f83e2926a6feea678d1ca1
artik3314@gmail.com <artik3314@gmail.com>
2022-12-14 19:42:06 +0300

    Создание архитектуры проекта

--- SportStoreMVVM/App.xaml.cs
+++ SportStoreMVVM/App.xaml.cs
@@ -8,10 +8,19 @@ using System.Windows;
 
 namespace SportStoreMVVM
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+    
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            SportStoreMVVM.MainWindow mv = new SportStoreMVVM.MainWindow();
+
+            //MainWindowViewModel vm = new MainWindowViewModel();
+            //mv.DataContext = vm;
+
+            mv.Show();
+        }
+
     }
 }
